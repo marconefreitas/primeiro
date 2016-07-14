010ff2e58491cd725afaa6eb555738effa5f1c2c
marconefreitas <marcfreitas0@gmail.com>
2016-07-14 17:54:08 -0300

    Primeiro Commit da Branch Dev

--- teste-git/src/main/java/Teste.java
+++ teste-git/src/main/java/Teste.java
@@ -3,5 +3,6 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("teste");
 		int a = 1;
+		System.out.println("Agora é um teste da Branch de desenvolvimento");
 	}
 }
